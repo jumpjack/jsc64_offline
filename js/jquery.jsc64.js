fe417ffce8d1b5b742dc7591e9dfda8bfaf97cfa
jumpjack <jumpjack@libero.it>
2024-10-21 13:37:41 +0200

    Update jquery.jsc64.js

--- js/jquery.jsc64.js
+++ js/jquery.jsc64.js
@@ -64,7 +64,10 @@ cachedKernalArr = arrayFromBinary(cachedKernal);
 console.log("kernalArr=",cachedKernalArr);
 cachedKernalBin =  binaryFromArray(cachedKernalArr);
 console.log("kernal2=", (cachedKernalBin === cachedKernal));
+cachedKernalBinFromFile = binaryFromArray(cachedKernalArrayFromFile);
+console.log("kernal3=", (cachedKernalBinFromFile === cachedKernal));
 jsc64Instance.romKernel = nl.kingsquare.as3.flash.utils.getByteArray(cachedKernalBinFromFile);
+console.log("KERNAL LOADED");
 
 // Carica e memorizza il contenuto del file BASIC
 cachedBasic = loadFileData(JSC64_BASEPATH + 'assets/basic.901226-01.bin', cachedBasic);
@@ -73,8 +76,11 @@ cachedBasicArr = arrayFromBinary(cachedBasic);
 console.log("cachedBasicArr=",cachedBasicArr);
 cachedBasicBin =  binaryFromArray(cachedBasicArr);
 console.log("basic2=", (cachedBasicBin === cachedBasic));
+cachedBasicBinFromFile = binaryFromArray(cachedBasicArrayFromFile);
+console.log("basic3=", (cachedBasicBinFromFile === cachedBasic));
 jsc64Instance.romBasic = nl.kingsquare.as3.flash.utils.getByteArray(cachedBasicBinFromFile);
-
+console.log("BASIC LOADED");
+			
 // Carica e memorizza il contenuto del file CHARS
 cachedChars = loadFileData(JSC64_BASEPATH + 'assets/characters.901225-01.bin', cachedChars);
 console.log("chars=",cachedChars.length);
@@ -82,7 +88,10 @@ cachedCharsArr = arrayFromBinary(cachedChars);
 console.log("cachedCharscArr=",cachedCharsArr);
 cachedCharsBin =  binaryFromArray(cachedCharsArr);
 console.log("chars2=", (cachedCharsBin === cachedChars));
+cachedCharsBinFromFile = binaryFromArray(cachedCharsArrayFromFile);
+console.log(chars3=", (cachedBasicBinFromFile === cachedChars));
 jsc64Instance.romChar = nl.kingsquare.as3.flash.utils.getByteArray(cachedCharsBinFromFile);
+console.log("CHARS LOADED");
 
 
 
